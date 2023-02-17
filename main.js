7ebfdace83abf63392348a397256936ad843887c
BharatiWagh <114480738+BharatiWagh@users.noreply.github.com>
2023-02-17 22:55:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function myfunction(){
+    window.location="https://www.makemytrip.com/";
+}
+function myfun(){
+    window.location="https://www.bus.irctc.co.in/home"
+}
+function myf(){
+    window.location="https://www.redbus.in/"
+}
+function my(){
+    window.history.back();
+}
